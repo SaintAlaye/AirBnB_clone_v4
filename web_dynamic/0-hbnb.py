2205811c4c5f7801c94da740ef2a46c12bf40edc
oberon10 <prosperbernrad@gmail.com>
2023-11-05 13:25:10 -0800

    updated

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 """
 Flask App that integrates with AirBnB static HTML Template
 """
+
 from flask import Flask, render_template, url_for
 from models import storage
 import uuid
